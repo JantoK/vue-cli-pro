fbee7442b9a9d66fb81a1f5556835538f7706c33
JantoK <JantoK@foxmail.com>
2020-12-03 17:22:41 +0800

    fix(main.js): move NProgress to router.js

--- src/main.js
+++ src/main.js
@@ -17,20 +17,6 @@ const req = require.context("@/assets/icons", false, /\.svg$/);
 const requireAll = requireContent => requireContent.keys().map(requireContent);
 requireAll(req);
 
-// 页面加载进度条
-import NProgress from "nprogress";
-import "nprogress/nprogress.css";
-
-// eslint-disable-next-line no-unused-vars
-router.beforeEach((to, from, next) => {
-  NProgress.start();
-  next();
-});
-
-router.afterEach(() => {
-  NProgress.done();
-});
-
 Vue.config.productionTip = false;
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
+// 页面加载进度条
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -25,6 +29,17 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+// 路由守卫
+// eslint-disable-next-line no-unused-vars
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
 });
 
 export default router;
