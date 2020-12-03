c3c255193331ea1cf0ecb8538b3101f6087956b0
JantoK <JantoK@foxmail.com>
2020-12-03 11:18:34 +0800

    feat(main.js): add nprogress

--- src/main.js
+++ src/main.js
@@ -2,7 +2,23 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+// 样式重制
 import "@/assets/stylus/reset.styl";
+
+// 页面加载进度条
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+// eslint-disable-next-line no-unused-vars
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
+});
 
 Vue.config.productionTip = false;
 
