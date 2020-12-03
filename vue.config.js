7314b3c692b66f86487d1e21bb530a83cc348c24
JantoK <JantoK@foxmail.com>
2020-12-03 17:49:37 +0800

    feat(vue.config.js): add webpack-bundle-analyzer and .env

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+// 代码分析工具
+const WebpackBundleAnalyzer = require("webpack-bundle-analyzer");
+// 路径处理
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
@@ -22,5 +25,11 @@ module.exports = {
         symbolId: "icon-[name]"
       })
       .end();
+    // 代码分析工具开启
+    if (process.env.USE_ANALYZER === "true") {
+      config
+        .plugin("webpack-bundle-analyzer")
+        .use(WebpackBundleAnalyzer.BundleAnalyzerPlugin);
+    }
   }
 };
